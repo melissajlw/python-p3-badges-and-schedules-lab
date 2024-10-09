b8072bd83df2fa10d220537fbd5948659a8a7b3d
Melissa Wang <melissa.wang21@gmail.com>
2024-10-08 23:20:47 -0400

    badges and schedules complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,21 @@
 def badge_maker(name):
-    return None
+    return (f"Hello, my name is {name}.")
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(badge_maker(name))
+    return badges
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    for i in range(len(names)):
+        rooms.append(f"Hello, {names[i]}! You'll be assigned to room {i + 1}!")
+    return rooms
 
 def printer(names):
-    return None
+    rooms = assign_rooms(names)
+    for name in names:
+        print(f"Hello, my name is {name}.")
+    for room in rooms:
+        print(room)
